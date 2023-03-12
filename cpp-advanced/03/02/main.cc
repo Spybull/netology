d2b5bcc800d8fdfa63b420d8ae3410837feb061a
spybull <danullbyte@gmail.com>
2023-03-12 20:10:48 +0300

    fixes

--- cpp-advanced/03/02/main.cc
+++ cpp-advanced/03/02/main.cc
@@ -1,16 +1,30 @@
 #include <iostream>
-#include <new>
 
-class smart_array {
-
-    int size, *ptr, count;
+class smart_array
+{
+    int size, count, *ptr;
 
     public:
-        smart_array(int size)
-        : size(size)
-        , ptr(new int[size]())
-        , count(0)
-        {}
+
+        smart_array(int size): size(size), count(0) {
+            if (size <= 0)
+                throw std::bad_alloc();
+
+            ptr = new int[size]();
+
+            if (ptr == nullptr)
+                throw std::bad_alloc();
+        }
+
+        smart_array(const smart_array &newArr) {
+
+            size = newArr.size;
+            count = newArr.count;
+            ptr = new int[size];
+
+            for (int i = 0; i < count; ++i)
+                ptr[i] = newArr[i];
+        }
 
         void add_element(int elem) {
             if ( count == size )
@@ -20,7 +34,7 @@ class smart_array {
         }
 
         int get_element(int elem) const {
-            if ( elem > (size - 1) || elem < 0 )
+            if ( elem >= size || elem < 0 )
                 throw std::range_error("Out of range");
 
             return ptr[elem];
@@ -30,18 +44,24 @@ class smart_array {
             return get_element(elem);
         }
 
-        void operator=(smart_array &newArr) {
+        smart_array &operator=(smart_array &newArr) {
 
-            delete []this->ptr;
-            this->size = newArr.size;
-            this->count = 0;
+            if (ptr == newArr.ptr)
+                return *this;
 
-            this->ptr = new int[newArr.size];
-            std::copy(newArr.ptr, newArr.ptr + newArr.size, this->ptr);
+            delete []ptr;
+            size = newArr.size;
+            count = newArr.count;
+            ptr = new int[size];
+            
+            for(int i = 0; i < count; ++i)
+                ptr[i] = newArr[i];
+
+            return *this;
         }
 
         void print() const {
-            for(int i = 0; i < size; ++i)
+            for(int i = 0; i < count; ++i)
                 std::cout << ptr[i] << " ";
             std::cout << std::endl;
         }
@@ -50,6 +70,7 @@ class smart_array {
             delete []ptr;
         }
 };
+
 
 int main(int argc, char const *argv[])
 {
@@ -62,9 +83,14 @@ int main(int argc, char const *argv[])
     new_array.add_element(44); 
     new_array.add_element(34);
 
-    std::cout << "Before: "; arr.print();
+
+    std::cout << "Before: ";
+    arr.print();
+
     arr = new_array;
-    std::cout << "After: "; arr.print();
+    
+    std::cout << "After: ";
+    arr.print();
 
     return 0;
 }

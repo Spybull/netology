f3dd1ade322489ab718430cb79f9ee32cbfd8522
spybull <danullbyte@gmail.com>
2023-03-31 09:48:26 +0300

    completed

--- cpp-advanced/course/main.cc
+++ cpp-advanced/course/main.cc
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <string_view>
-#include <vector>
 #include <map>
 #include <exception>
 #include <algorithm>
@@ -11,7 +9,6 @@ typedef std::map<std::string, std::string> map_ss;
 class ini_exceptions : public std::exception {
     private:
         int posInLine;
-        int posInString;
         std::string message;
 
     public:
@@ -38,13 +35,26 @@ class ini_parser {
         size_t line_count;
         size_t section_count;
         size_t kv_count;
-        std::map< std::string, std::map <std::string, std::string > > section;
+        std::map< std::string, map_ss > section;
 
-        bool isKv(std::string &kv) {
+        inline bool is_number(const std::string& s) {
+
+            std::string::const_iterator it = s.cbegin();
+            int point = 0;
+            while ( (it != s.end() && std::isdigit(*it)) || *it == '.') {
+                if (*it == '.') ++point;
+                 ++it;
+            }
+
+            if ( point > 1 )
+                return false;
+
+            return !s.empty() && it == s.end();
+        }
+        inline bool isKv(const std::string &kv) const {
             return kv.find_first_of('=') != std::string::npos ?
                    true : false;
         }
-
         inline std::string trim(const std::string &str) {
 
             if (!str.length())
@@ -66,7 +76,7 @@ class ini_parser {
                    section[section.length() - 1] == ']' ? 
                    true : false;
         }
-        bool isValidSection(std::string &section) {
+        bool isValidSection(const std::string &section) const{
 
              std::size_t len = section.length();
 
@@ -87,7 +97,7 @@ class ini_parser {
 
             return true;
         }
-        std::string getSectionName(std::string &section) {
+        std::string getSectionName(const std::string &section) {
             return section.substr(1, section.length() - 2);
         }
 
@@ -103,28 +113,35 @@ class ini_parser {
                 throw std::runtime_error("Could not open file...");
 
             std::string line, current_section;
-            bool sectionFound = false;
             while (std::getline(In, line))
             {
                 size_t pp = line.find_first_of(';');
                 if ( pp != std::string::npos )
-                    line = line.erase(pp); //move
+                    line = line.erase(pp);
 
                 line = trim(line);
                 ++line_count;
 
                 // Skip blank lines
-                if ( ! line.length() ) continue;
+                if ( ! line.length() )
+                    continue;
 
                 /* search sections first */
                 if ( isSection(line) && isValidSection(line) ) {
 
                     ++section_count;
                     current_section = trim(getSectionName(line));
-                    if (section.find(current_section) == section.end())
+
+                    std::transform(current_section.begin(),
+                                   current_section.end(),
+                                   current_section.begin(),
+                                   ::tolower);
+
+                    if (section.find(current_section) == section.end()) {
                         section[current_section] = {};
-                }
-                else if ( isKv(line) ) {
+                    }
+
+                } else if ( isKv(line) ) {
 
                     ++kv_count;
                     if ( !section_count )
@@ -138,36 +155,11 @@ class ini_parser {
 
                     std::string value = trim(line.substr(eqPos + 1));
                     section[current_section].insert_or_assign(key, value);
-                } else {
-                    throw ini_exceptions(line_count, "It's not a key, just string");
                 }
+                else
+                    throw ini_exceptions(line_count, "It's not a key, just string");
  
             } // end of main loop
-        }
-
-        inline bool is_number(const std::string& s) {
-
-            std::string::const_iterator it = s.begin();
-            int point = 0;
-            while ( (it != s.end() && std::isdigit(*it)) || *it == '.') {
-                if (*it == '.') ++point;
-                 ++it;
-            }
-
-            if ( point > 1 )
-                return false;
-
-            return !s.empty() && it == s.end();
-        }
-
-        void chekcOUT(const std::string &str, const std::type_info &type)
-        {
-            bool n = is_number(str);
-            if (n && typeid(std::string) == type )
-                throw std::runtime_error("Wrong requested type of string!");
-            
-            if ( !n && typeid(std::string) != type )
-                throw std::runtime_error("Wrong types!");
         }
 
         template<typename T>
@@ -177,16 +169,21 @@ class ini_parser {
             if ( pp == std::string::npos )
                 throw std::runtime_error("Unknown section name");
 
-            auto it = section.find(std::move(str_val.substr(0, pp)));
+            std::string current_section = str_val.substr(0, pp);
+            std::transform(current_section.begin(),
+                current_section.end(),
+                current_section.begin(),
+                ::tolower);
+
+            auto it = section.find(current_section);
             if ( it == section.end() )
                 throw std::runtime_error("Section not found");
 
-            std::string finding_key = std::move(str_val.substr(pp + 1));
+            std::string finding_key = str_val.substr(pp + 1);
             auto it_kv = it->second.find(finding_key);
 
             if ( it_kv == it->second.end() )
                 throw ini_exceptions(finding_key, it->second);
-                // throw std::runtime_error("Key not found");
 
             bool n = is_number(it_kv->second);
             if (n && typeid(std::string) == typeid(T) )
@@ -196,29 +193,32 @@ class ini_parser {
                 throw std::runtime_error("Wrong types!");
 
             if constexpr(std::is_integral<T>::value)
-                return atoi(it_kv->second.c_str());
+                return std::atoi(it_kv->second.c_str());
             else if constexpr(std::is_floating_point<T>::value)
-                return atof(it_kv->second.c_str());
-            else if constexpr(std::is_base_of<std::string, T>::value)
+                return std::atof(it_kv->second.c_str());
+            else if constexpr(std::is_base_of<char, T>::value)
+                return it_kv->second;
+            else
                 return it_kv->second;
         }
 
         ini_parser(const ini_parser &) = delete;
         ini_parser(ini_parser &&) = delete;
-        ~ini_parser() = default;
-
         ini_parser &operator=(const ini_parser &) = delete;
         ini_parser &operator=(ini_parser &&) = delete;
+        ~ini_parser() = default;
 };
 
 
-int main(int argc, char const *argv[])
+int main()
 {
     try
     {
         ini_parser parser("test.ini");
-        std::cout << parser.get_value<float>("Section1.var6") << std::endl;
-    } catch (std::exception &e) {
+        std::cout << parser.get_value<std::string>("section2.var4") << std::endl;
+    }
+    catch (const std::exception &e)
+    {
         std::cout << e.what() << std::endl;
     }
     return 0;
